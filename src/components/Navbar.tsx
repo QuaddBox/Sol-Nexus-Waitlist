4b03cabc1ef954020d5fdf29663506b402d58aa9
Wutche <wuthchechikaome75@gmail.com>
2024-03-24 23:51:26 -0700

    done with some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,14 @@
 
 import { NavLink } from "react-router-dom";
 import logo from "../assets/solnexus.png";
-import { Image } from "@mantine/core";
+
+import { Image, Button, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 import { RiMenu2Line } from "react-icons/ri";
 import { LiaTimesSolid } from "react-icons/lia";
+import { FaSpotify } from "react-icons/fa";
+
 import Navmenu from "./Navmenu";
 import { useState } from "react";
 
@@ -16,6 +20,8 @@ const Navbar = () => {
 		setMenu((prevmenu) => !prevmenu);
 	};
 
+	const [opened, { open, close }] = useDisclosure(false);
+
 	return (
 		<nav>
 			<div className="navcontents">
@@ -24,14 +30,48 @@ const Navbar = () => {
 						<Image src={logo} width={"80px"} h={"80px"} />
 					</NavLink>
 				</div>
-				<div className="navlinks">
-					<NavLink to={"about"}>About Us</NavLink>
 
-					<NavLink
-						to={"https://solnexus.gitbook.io/solnexus/"}
-						className="whitepaper">
-						Whitepaper
-					</NavLink>
+				<div className="navwrp">
+					<Modal
+						opened={opened}
+						onClose={close}
+						centered
+						transitionProps={{
+							transition: "fade",
+							duration: 600,
+							timingFunction: "linear",
+						}}
+						className="modal">
+						<h1 className="modalhead-text">
+							Listen to our dev's playlist while you wait
+						</h1>
+						<div className="modalbtn">
+							<iframe
+								src="https://open.spotify.com/embed/playlist/5e7S1ZKrEpBqoxyOG4opBl?utm_source=generator&theme=0"
+								width="100%"
+								height="352"
+								frameBorder="0"
+								allowfullscreen=""
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy">
+								Listen Now
+							</iframe>
+						</div>
+					</Modal>
+					<div className="spotify">
+						<Button bg={"none"} onClick={open}>
+							<FaSpotify fontSize={"25px"} color="#1DB954" cursor={"pointer"} />
+						</Button>
+					</div>
+					<div className="navlinks">
+						<NavLink to={"about"}>About Us</NavLink>
+
+						<NavLink
+							to={"https://solnexus.gitbook.io/solnexus/"}
+							className="whitepaper">
+							Whitepaper
+						</NavLink>
+					</div>
 				</div>
 				<div className="navmenu-icon" onClick={showMenu}>
 					{!menu ? <RiMenu2Line /> : <LiaTimesSolid />}
