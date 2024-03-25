e767ea748f3d15f3dcb9a123c466b9ff472bd481
Wutche <wuthchechikaome75@gmail.com>
2024-03-25 00:13:15 -0700

    done with some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,7 +51,6 @@ const Navbar = () => {
 								width="100%"
 								height="352"
 								frameBorder="0"
-								allowfullscreen=""
 								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 								loading="lazy"></iframe>
 						</div>
